1dfc7cd738efa175b129048d0cabfb8077e822cc
Cristhian Molina <cdml2001@hotmail.com>
2025-04-03 16:27:49 -0400

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,9 @@
+import Text from "@/components/Text/UI/Text";
+import { DarkTheme, LightTheme } from "@/constants/colorScheme";
 import { Icon, MaterialIconName } from "@roninoss/icons";
 import { router } from "expo-router";
-import {
-  FlatList,
-  Pressable,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { FlatList, Pressable, useColorScheme, View } from "react-native";
+import { StyleSheet } from "react-native-unistyles";
 
 interface dataComponent {
   name: string;
@@ -34,37 +30,45 @@ const data: dataComponent[] = [
 ];
 
 export default function Page() {
+  const theme = useColorScheme();
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.container}>
-        <Text style={styles.title}>Components</Text>
-        <FlatList
-          data={data}
-          renderItem={({ item }) => (
-            <Pressable
-              style={styles.itemContainer}
-              onPress={() => router.push(item.path)}
-            >
-              <Icon name={item.icon} size={24} color="black" />
-              <Text style={styles.itemName}>{item.name}</Text>
-            </Pressable>
-          )}
-        />
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text fontVariant="title1" weight="bold">
+        Components
+      </Text>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <Pressable
+            style={styles.itemContainer}
+            onPress={() => router.push(item.path)}
+          >
+            <Icon
+              name={item.icon}
+              size={24}
+              color={
+                theme === "dark"
+                  ? DarkTheme.colors.text
+                  : LightTheme.colors.text
+              }
+            />
+            <Text>{item.name}</Text>
+          </Pressable>
+        )}
+      />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create((theme) => ({
+  outContainer: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     padding: 16,
-  },
-  title: {
-    fontSize: 32,
-    textAlign: "center",
-    fontWeight: "bold",
-    marginBottom: 16,
+    backgroundColor: theme.colors.background,
   },
   itemContainer: {
     flexDirection: "row",
@@ -73,10 +77,12 @@ const styles = StyleSheet.create({
     padding: 16,
     borderWidth: 2,
     borderRadius: 8,
+    borderColor: theme.colors.border,
+    gap: 8,
   },
   itemName: {
     fontSize: 18,
     marginLeft: 16,
     fontWeight: "bold",
   },
-});
+}));
