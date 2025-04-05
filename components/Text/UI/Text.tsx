97bfbea024b4f80627dfc7d8dc1c870fbc488786
Cristhian Molina <cdml2001@hotmail.com>
2025-04-04 23:25:42 -0400

    feat: added tablet support on text

--- components/Text/UI/Text.tsx
+++ components/Text/UI/Text.tsx
@@ -28,14 +28,14 @@ export default function Text({
     textAlign: textAlign,
   });
 
-  const text = StyleSheet.flatten([
+  const textStyle = StyleSheet.flatten([
     styles.text,
     isCustomColor ? { color } : {},
     style,
   ]);
 
   return (
-    <RNText style={text} {...rest}>
+    <RNText style={textStyle} {...rest}>
       {children}
     </RNText>
   );
@@ -46,66 +46,156 @@ const styles = StyleSheet.create((theme) => ({
     variants: {
       fontVariant: {
         largeTitle: {
-          fontSize: 34,
-          fontWeight: 700,
-          lineHeight: 41,
-          letterSpacing: 0.37,
+          fontSize: {
+            phone: 34,
+            tablet: 44,
+          },
+          fontWeight: {
+            phone: 700,
+            tablet: 800,
+          },
+          lineHeight: {
+            phone: 41,
+            tablet: 52,
+          },
+          letterSpacing: {
+            phone: 0.37,
+            tablet: 0.46,
+          },
         },
         title1: {
-          fontSize: 28,
-          lineHeight: 34,
-          letterSpacing: 0.36,
+          fontSize: {
+            phone: 28,
+            tablet: 34,
+          },
+          lineHeight: {
+            phone: 34,
+            tablet: 41,
+          },
+          letterSpacing: {
+            phone: 0.36,
+            tablet: 0.43,
+          },
         },
         title2: {
-          fontSize: 22,
-          lineHeight: 28,
-          letterSpacing: 0.35,
+          fontSize: {
+            phone: 22,
+            tablet: 28,
+          },
+          lineHeight: {
+            phone: 28,
+            tablet: 34,
+          },
+          letterSpacing: {
+            phone: 0.35,
+            tablet: 0.42,
+          },
         },
         title3: {
-          fontSize: 20,
-          lineHeight: 24,
-          letterSpacing: 0.34,
+          fontSize: {
+            phone: 20,
+            tablet: 24,
+          },
+          lineHeight: {
+            phone: 24,
+            tablet: 28,
+          },
+          letterSpacing: {
+            phone: 0.34,
+            tablet: 0.41,
+          },
         },
         headline: {
-          fontSize: 17,
-          fontWeight: 600,
-          lineHeight: 22,
-          letterSpacing: 0.15,
+          fontSize: {
+            phone: 17,
+            tablet: 20,
+          },
+          fontWeight: {
+            phone: 600,
+            tablet: 700,
+          },
+          lineHeight: {
+            phone: 22,
+            tablet: 28,
+          },
+          letterSpacing: {
+            phone: 0.15,
+            tablet: 0.18,
+          },
         },
         body: {
-          fontSize: 17,
-          lineHeight: 22,
+          fontSize: {
+            phone: 17,
+            tablet: 20,
+          },
+          lineHeight: {
+            phone: 22,
+            tablet: 26,
+          },
           letterSpacing: 0.15,
         },
         callout: {
-          fontSize: 16,
-          lineHeight: 20,
-          letterSpacing: 0.15,
+          fontSize: {
+            phone: 16,
+            tablet: 19,
+          },
+          lineHeight: {
+            phone: 22,
+            tablet: 24,
+          },
+          letterSpacing: {
+            phone: 0.15,
+            tablet: 0.18,
+          },
         },
         subheadline: {
-          fontSize: 15,
-          lineHeight: 20,
+          fontSize: {
+            phone: 15,
+            tablet: 17,
+          },
+          lineHeight: {
+            phone: 20,
+            tablet: 24,
+          },
           letterSpacing: 0.15,
         },
         footnote: {
-          fontSize: 13,
-          lineHeight: 18,
+          fontSize: {
+            phone: 13,
+            tablet: 15,
+          },
+          lineHeight: {
+            phone: 18,
+            tablet: 20,
+          },
           letterSpacing: 0.1,
         },
         caption1: {
-          fontSize: 12,
-          lineHeight: 16,
+          fontSize: {
+            phone: 12,
+            tablet: 13,
+          },
+          lineHeight: {
+            phone: 16,
+            tablet: 18,
+          },
           letterSpacing: 0.1,
         },
         caption2: {
-          fontSize: 11,
-          lineHeight: 16,
+          fontSize: {
+            phone: 11,
+            tablet: 12,
+          },
+          lineHeight: {
+            phone: 16,
+            tablet: 18,
+          },
           letterSpacing: 0.1,
         },
       },
       weight: {
         light: {
-          fontWeight: 300,
+          fontWeight: 200,
         },
         medium: {
           fontWeight: 500,
